5f3a345a0b7819d91b45e0fabb295c89ec78677f
mayesse <58993065+mayesse@users.noreply.github.com>
2026-06-09 19:03:08 +0200

    Update download page version to 2.1.9

--- src/landing/pages/DownloadPage.jsx
+++ src/landing/pages/DownloadPage.jsx
@@ -2,7 +2,7 @@ import { motion } from 'framer-motion'
 import { useTranslation } from 'react-i18next'
 import { ArrowDownTrayIcon, ComputerDesktopIcon, DevicePhoneMobileIcon } from '@heroicons/react/24/outline'
 
-const APP_VERSION = '2.1.8'
+const APP_VERSION = '2.1.9'
 
 export default function DownloadPage() {
   const { t } = useTranslation()
@@ -91,3 +91,4 @@ export default function DownloadPage() {
     </div>
   )
 }
+
